2cf0ffb65b67dc5b00d73224d2704e8470b0a1a9
Orlan <orlantquijada@gmail.com>
2025-03-30 02:16:01 +0800

    Remove types

--- packages/db/tsup.config.ts
+++ packages/db/tsup.config.ts
@@ -4,6 +4,5 @@ export default defineConfig((options) => ({
 	entry: ["./src/index.ts", "./src/client.ts", "./src/schema.ts"],
 	format: "esm",
 	sourcemap: true,
-	dts: true,
 	minify: !options.watch,
 }));
--- packages/utils/tsup.config.ts
+++ packages/utils/tsup.config.ts
@@ -4,6 +4,5 @@ export default defineConfig((options) => ({
 	entry: ["src/index.ts"],
 	format: "esm",
 	sourcemap: true,
-	dts: true,
 	minify: !options.watch,
 }));
