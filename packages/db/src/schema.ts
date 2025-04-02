0cf044aa73102c9a2ce6ac5eef13f4b5ab1e1227
TheTanTanAwit <awitnathan@gmail.com>
2025-04-02 11:30:36 +0800

    added changes from comments

--- packages/db/src/schema.ts
+++ packages/db/src/schema.ts
@@ -1,5 +1,6 @@
+import { match } from "assert";
 import { sql } from "drizzle-orm";
-import { pgTable } from "drizzle-orm/pg-core";
+import { pgEnum, pgTable } from "drizzle-orm/pg-core";
 import { createInsertSchema } from "drizzle-valibot";
 import * as v from "valibot";
 
@@ -19,76 +20,115 @@ export const CreatePostSchema = v.pick(
 	["title"],
 );
 
+export const userRoles = ["Consumer", "Producer", "Logistics"] as const;
+const rolesEnum = pgEnum("rolesEnum", userRoles);
+
 export const User = pgTable("users", (t) => ({
-    id: t.uuid().notNull().primaryKey().defaultRandom(),
-    name: t.varchar({ length: 255 }).notNull(),
-    email: t.varchar({ length: 255 }).notNull(),
-    passwordHash: t.varchar({ length: 255 }).notNull(),
-    phoneNumber: t.varchar({ length: 20 }),
-    role: t.varchar({ length: 20, enum: ["Consumer", "Producer", "Logistics"] }).notNull(),
-    location: t.point().notNull(),
-    createdAt: t.timestamp().defaultNow().notNull()
- 
+	id: t.uuid().notNull().primaryKey().defaultRandom(),
+	name: t.varchar({ length: 255 }).notNull(),
+	email: t.varchar({ length: 255 }).notNull(),
+	passwordHash: t.varchar({ length: 255 }).notNull(),
+	phoneNumber: t.varchar({ length: 20 }),
+	role: rolesEnum().notNull(),
+	location: t.point().notNull(),
+	createdAt: t.timestamp().defaultNow().notNull(),
 }));
- 
-export const Produce_Listing = pgTable("produceListings", (t) => ({
-    id: t.uuid().notNull().primaryKey().defaultRandom(),
-    farmerId: t.integer().references(() => User.id),
-    produceName: t.varchar({ length: 255 }).notNull(),
-    variety: t.varchar({ length: 255 }).notNull(),
-    quantity: t.real().notNull(),
-    pricePerKg: t.real().notNull(),
-    status: t.varchar({ length: 20, enum: ["available", "partially_sold", "sold"]}).notNull(),
-    harvestDate: t.date(),
-    expirationDate: t.date(),
-    location: t.point().notNull(),
-    createdAt: t.timestamp().defaultNow().notNull()
+
+export const produceListingStatus = [
+	"available",
+	"partially_sold",
+	"sold",
+] as const;
+const produceListingStatusEnum = pgEnum(
+	"produce_listing_status_enum",
+	produceListingStatus,
+);
+
+export const ProduceListing = pgTable("produce_listings", (t) => ({
+	id: t.uuid().notNull().primaryKey().defaultRandom(),
+	farmerId: t.integer().references(() => User.id),
+	produceName: t.varchar({ length: 255 }).notNull(),
+	variety: t.varchar({ length: 255 }).notNull(),
+	quantity: t.numeric({ mode: "number", precision: 12, scale: 4 }).notNull(),
+	pricePerKg: t.numeric({ mode: "number", precision: 12, scale: 2 }).notNull(),
+	status: produceListingStatusEnum().notNull(),
+	harvestDate: t.date(),
+	expirationDate: t.date(),
+	location: t.point().notNull(),
+	createdAt: t.timestamp().defaultNow().notNull(),
 }));
- 
-export const Buy_Order = pgTable("buyOrders", (t) => ({
-    id: t.uuid().notNull().primaryKey().defaultRandom(),
-    buyerId: t.integer().references(() => User.id),
-    produceName: t.varchar({ length: 255 }).notNull(),
-    quantity: t.real().notNull(),
-    minPricePerKg: t.real(),
-    maxPricePerKg: t.real(),
-    status: t.varchar({ length: 20, enum: ["pending", "matched", "completed", "cancelled"]}).notNull(),
-    deliveryLocation: t.point().notNull(),
-    createdAt: t.timestamp().defaultNow().notNull()
-}))
- 
+
+export const buyOrderStatus = [
+	"pending",
+	"matched",
+	"completed",
+	"cancelled",
+] as const;
+const buyOrderEnum = pgEnum("buy_order_enum", buyOrderStatus);
+
+export const BuyOrder = pgTable("buyOrders", (t) => ({
+	id: t.uuid().notNull().primaryKey().defaultRandom(),
+	buyerId: t.integer().references(() => User.id),
+	produceName: t.varchar({ length: 255 }).notNull(),
+	quantity: t.numeric({ mode: "number", precision: 12, scale: 4 }).notNull(),
+	minPricePerKg: t.numeric({ mode: "number", precision: 8, scale: 2 }),
+	maxPricePerKg: t.numeric({ mode: "number", precision: 8, scale: 2 }),
+	status: buyOrderEnum().notNull(),
+	deliveryLocation: t.point().notNull(),
+	createdAt: t.timestamp().defaultNow().notNull(),
+}));
+
+export const matchStatus = [
+	"pending",
+	"confirmed",
+	"shipped",
+	"delivered",
+] as const;
+const matchEnum = pgEnum("matchEnum", matchStatus);
+
 export const Match = pgTable("matches", (t) => ({
-    id: t.uuid().primaryKey().defaultRandom(),
-    produceListingId: t.integer().references(() => Produce_Listing.id),
-    buyOrderId: t.integer().references(() => Buy_Order.id),
-    matchedQuantityInKg: t.real().notNull(),
-    consumerId: t.integer().references(() => User.id),
-    producerId: t.integer().references(() => User.id),
-    status: t.varchar({ length: 20, enum: ["pending", "confirmed", "shipped", "delivered"] }).notNull(),
-    createdAt: t.timestamp().defaultNow().notNull()
-}))
- 
+	id: t.uuid().primaryKey().defaultRandom(),
+	produceListingId: t.integer().references(() => ProduceListing.id),
+	buyOrderId: t.integer().references(() => BuyOrder.id),
+	matchedQuantityInKg: t
+		.numeric({ mode: "number", precision: 16, scale: 4 })
+		.notNull(),
+	consumerId: t.integer().references(() => User.id),
+	producerId: t.integer().references(() => User.id),
+	status: matchEnum().notNull(),
+	createdAt: t.timestamp().defaultNow().notNull(),
+}));
+
+export const transactionStatus = ["pending", "paid", "failed"] as const;
+const transactionEnum = pgEnum("transactionEnum", transactionStatus);
+
 export const Transaction = pgTable("transactions", (t) => ({
-    id: t.uuid().primaryKey().defaultRandom(),
-    matchId: t.integer().references(() => Match.id),
-    amount: t.numeric({ precision: 10, scale: 4}),
-    paymentMethod: t.varchar({ length: 20 }).notNull(),
-    status: t.varchar({ length: 20, enum: ["pending", "paid", "failed"]}),
-    createdAt: t.timestamp().defaultNow().notNull()
-}))
- 
+	id: t.uuid().primaryKey().defaultRandom(),
+	matchId: t.integer().references(() => Match.id),
+	amount: t.numeric({ precision: 10, scale: 4 }),
+	paymentMethod: t.varchar({ length: 20 }).notNull(),
+	status: transactionEnum().notNull(),
+	createdAt: t.timestamp().defaultNow().notNull(),
+}));
+
+export const logisticsStatus = ["pending", "in transit", "delivered"] as const;
+const logisticsEnum = pgEnum("logisticsEnum", logisticsStatus);
+
 export const Logistics = pgTable("logistics", (t) => ({
-    id: t.uuid().primaryKey().defaultRandom(),
-    matchId: t.integer().references(() => Match.id),
-    truckId: t.integer().references(() => Truck.id),
-    driverId: t.integer().references(() => User.id),
-    status: t.varchar({ length: 20, enum: ["pending", "in transit", "delivered"]})
-}))
- 
+	id: t.uuid().primaryKey().defaultRandom(),
+	matchId: t.integer().references(() => Match.id),
+	truckId: t.integer().references(() => Truck.id),
+	driverId: t.integer().references(() => User.id),
+	status: logisticsEnum().notNull(),
+}));
+
+export const truckStatus = ["available", "in use", "maintenance"] as const;
+const truckEnum = pgEnum("truckEnum", truckStatus);
+
 export const Truck = pgTable("trucks", (t) => ({
-    id: t.uuid().primaryKey().defaultRandom(),
-    plateNumber: t.varchar({ length: 20 }).notNull(),
-    capacityKg: t.numeric({ precision: 10, scale: 2}),
-    status: t.varchar({ length: 20, enum: ["available", "in use", "maintenance"]}),
-    createdAt: t.timestamp().defaultNow().notNull()
-}))
+	id: t.uuid().primaryKey().defaultRandom(),
+	plateNumber: t.varchar({ length: 20 }).notNull(),
+	capacityKg: t.numeric({ precision: 10, scale: 2 }),
+	status: truckEnum().notNull(),
+	createdAt: t.timestamp().defaultNow().notNull(),
+}));
