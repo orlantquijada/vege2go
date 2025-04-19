2d255d8ed30a57c6cb12dffc510505822a44605f
Orlan <orlantquijada@gmail.com>
2025-04-19 18:36:31 +0800

    Fix lint CI

--- apps/web-app/app/routes/home.tsx
+++ apps/web-app/app/routes/home.tsx
@@ -10,7 +10,7 @@ export async function loader() {
 	return { posts };
 }
 
-export function meta({}: Route.MetaArgs) {
+export function meta(_: Route.MetaArgs) {
 	return [
 		{ title: "New React Router App" },
 		{ name: "description", content: "Welcome to React Router!" },
